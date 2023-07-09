eb371d2da138a057299b231ab05d873ff4ee5e08
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-07-09 10:02:37 +0600

    deleted oparation

--- index.js
+++ index.js
@@ -78,6 +78,16 @@ async function run() {
       res.send(result);
     });
 
+    // delete toy
+    app.delete("/alltoy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await allToyData.deleteOne(query);
+      res.send(result);
+    });
+
+    // update a toy
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
